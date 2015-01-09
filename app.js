231bc3df87629506587617ecc4fb3bd03f13c94f
Timilehin Aderinola <timilehin.aderinola@andela.co>
2015-01-09 14:20:40 +0100

    moved the function 'allowCrossDomain'

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+//Cross domain allow access
+var allowCrossDomain = function(request, response, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  next();
+};
+
+app.use(allowCrossDomain);
+
 app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static('public'));
@@ -11,17 +22,6 @@ app.use(bodyParser.json());
 
 var todos = require('./routes/todos');
 app.use('/todos', todos);
-
-//Cross domain allow access
-var allowCrossDomain = function(request, response, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-	next();
-};
-
-app.use(allowCrossDomain);
 
 app.listen(app.get('port'), function() {
   console.log("Listening on port 5000");
