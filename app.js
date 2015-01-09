ecadb738912d92104758e17a0757ef306ed27acc
BABAJIDE FOWOTADE <babajide.fowotade@andela.co>
2015-01-09 08:10:34 +0100

    File updated

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000))
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -8,9 +10,6 @@ app.use(bodyParser.json());
 var todos = require('./routes/todos');
 app.use('/todos', todos);
 
-
-
-
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
   console.log("Listening on port 3000");
 });
