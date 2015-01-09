cf6210465c847d5015913c66e0653b8b237c3d26
BABAJIDE FOWOTADE <babajide.fowotade@andela.co>
2015-01-09 08:06:46 +0100

    Route Redirect

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 5000))
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
